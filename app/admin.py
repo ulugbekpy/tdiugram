fd119ab64d6ea574c1fe8d70a4bb9b668b3298e9
ulugbekpy <ulugbekpycpp@gmail.com>
2023-03-12 15:09:15 +0500

    Some models have been written

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Foydalanuvchi
 
-# Register your models here.
+admin.site.register(Foydalanuvchi
+                    )
--- app/models.py
+++ app/models.py
@@ -12,3 +12,24 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+
+class Foydalanuvchi(User):
+
+    class StatusChioce(models.TextChoices):
+        STUDENT = 'S'
+        TEACHER = 'T'
+        GUEST = 'G'
+
+    phone = models.CharField(max_length=15, unique=True)
+    status = models.CharField(
+        max_length=1, choices=StatusChioce.choices, default=StatusChioce.GUEST)
+    image = models.ImageField(upload_to='users/', blank=True, null=True)
+
+
+class Comment(models.Model):
+    sender = models.ForeignKey(Foydalanuvchi, on_delete=models.PROTECT)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_sent = models.DateTimeField(auto_now=True)
+    date_updated = models.DateTimeField(auto_now_add=True)
