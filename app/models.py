ae6f13260046771033bd886f2499664c5da0d939
ulugbekpy <ulugbekpycpp@gmail.com>
2023-03-12 05:20:28 +0500

    Basic things

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)
+    content = models.TextField()
+    sent_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
---
+++ makefile
@@ -0,0 +1,7 @@
+mig:
+	python manage.py makemigrations
+	python manage.py migrate
+
+
+run:
+	python manage.py runserver
